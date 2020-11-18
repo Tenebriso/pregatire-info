dd776d2383c250ff8516bdb98c9337b695cb9107
Tenebriso <evoicu@adobe.com>
2020-11-18 18:30:46 +0200

    fix coding style for submultimi.cpp

--- alberto/submultimi.cpp
+++ alberto/submultimi.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 using namespace std;
-int x[20],n,p;
-void afisare(int k){
-    for(int i=1;i<=k;i++)
-        cout<<x[i]<<" ";
-    cout<<endl;
+
+int solutie[20], n;
+
+void afisare(int pas_curent) {
+    for (int i = 1; i <= pas_curent; i++)
+        cout << solutie[i] << " ";
+    cout << endl;
 }
-int cont(int k)
+
+int cont(int pas_curent)
 {
-    if (x[k]>x[k-1]) return 1;
-return 0;
+    if (solutie[pas_curent] > solutie[pas_curent - 1]) {
+	    return 1;
+    }
+    return 0;
 }
-void back(int k){
-   for (int i=1;i<=n;i++){
-        x[k]=i;
-    if(cont (k)) {
-      back(k+1);
-    afisare(k);
-  }}
+
+void back(int pas_curent){
+   for (int i = 1; i <= n; i++) {
+        solutie[pas_curent] = i;
+        if (cont(pas_curent)) {
+	      back(pas_curent + 1);
+	     afisare(pas_curent);
+  	}
+   }
 }
+
 int main()
 {
-    cout<<"n=";cin>>n;
+    cout << "n=";
+    cin >> n;
+    //afisare multime vida
+    cout << endl;
+
     back(1);
 }
